c4537f746aa3c2751926aa6b87423d681df209d2
David Yang <daworld.ny@gmail.com>
2015-12-17 18:05:25 +0500

    Removed legacy parts

--- Controller/Admin/File/AbstractFile.php
+++ Controller/Admin/File/AbstractFile.php
@@ -12,76 +12,17 @@
 namespace Videogallery\Controller\Admin\File;
 
 use Admin\Controller\Admin\AbstractController;
-use stdclass;
 
 abstract class AbstractFile extends AbstractController
 {
     /**
-     * Returns request container
-     * 
-     * @return \stdclass
-     */
-    final protected function getContainer()
-    {
-        $container = new stdclass();
-        
-        // Data
-        $container->name = $this->request->getPost('name');
-        $container->title = $this->request->getPost('title');
-        
-        // Files
-        $container->coverFile = $this->request->getFiles('coverFile');
-        $container->videoFile = $this->request->getFiles('videoFile');
-        
-        return $container;
-    }
-
-    /**
-     * Returns template path
-     * 
-     * @return string
-     */
-    final protected function getTemplatePath()
-    {
-        return 'file.form';
-    }
-
-    /**
-     * Returns file manager
-     * 
-     * @return \Videogallery\Service\FileManager
-     */
-    final protected function getFIleManager()
-    {
-        return $this->moduleManager->getModule('Videogallery')->getService('fileManager');
-    }
-
-    /**
-     * Loads shared plugins
+     * Load shared view plugins
      * 
      * @return void
      */
     final protected function loadSharedPlugins()
     {
-        $this->viewModel->loadPlugin('ckeditor');
+        $this->view->getPluginBag()
+                   ->load($this->getWysiwygPluginName());
     }
-
-    /**
-     * Return shared variables
-     * 
-     * @param array $overrides
-     * @return array
-     */
-    final protected function getSharedVars(array $overrides)
-    {
-        $vars = array(
-            'breadcrumbs' => array(
-                'Videogallery:Admin:Browser@indexAction' => 'Videogallery'
-            )
-        );
-        
-        return array_replace_recursive($vars, $overrides);
-    }
-    
-    
 }
