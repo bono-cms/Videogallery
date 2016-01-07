c8ab6e571a10679f741840d1e53568c587215bea
David Yang <daworld.ny@gmail.com>
2016-01-07 13:51:51 +0500

    Fixes in skeletons

--- Controller/Category.php
+++ Controller/Category.php
@@ -1,25 +1,36 @@
 <?php
 
-final class Category
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace News\Controller;
+
+use Site\Controller\AbstractController;
+
+final class Category extends AbstractController
 {
     /**
-     * Lists all categories
+     * Renders a category by its associated id
+     * 
+     * @param string $id
+     * @return string
+     */
+    public function indexAction($id)
+    {
+    }
+
+    /**
+     * Renders all categories
      * 
      * @return string
      */
     public function listAction()
     {
-        
-    }
-
-    /**
-     * Views a category by its associated id
-     * 
-     * @param string $id
-     * @return string
-     */
-    public function viewAction($id)
-    {
-        
     }
 }
--- Controller/File.php
+++ Controller/File.php
@@ -1,17 +1,27 @@
 <?php
 
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
 
+namespace News\Controller;
 
-final class File
+use Site\Controller\AbstractController;
+
+final class File extends AbstractController
 {
     /**
-     * Views video file by its id
+     * Displays a video record by its associated id
      * 
      * @param string $id
      * @return string
      */
-    public function viewAction($id)
+    public function indexAction($id)
     {
-        
     }
 }
