8f48d8a8a80925b7bb075ff9902105d9afa29b44
David Yang <daworld.ny@gmail.com>
2016-01-28 04:04:02 +0500

    Fixed column names

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS `bono_module_videogallery_files`;
 CREATE TABLE `bono_module_videogallery_files` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`lang_id` INT NOT NULL,
-	`categoryId` INT NOT NULL,
+	`category_id` INT NOT NULL,
 	`title` varchar(254) NOT NULL,
 	`name` varchar(254) NOT NULL,
 	`description` TEXT NOT NULL,
-	`metaDescription` TEXT NOT NULL,
+	`meta_description` TEXT NOT NULL,
 	`keywords` TEXT NOT NULL
 	
 ) DEFAULT CHARSET = UTF8;
@@ -15,8 +15,8 @@ CREATE TABLE `bono_module_videogallery_files` (
 DROP TABLE IF EXISTS `bono_module_videogallery_categories`;
 CREATE TABLE `bono_module_videogallery_categories` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`langId` INT NOT NULL,
-	`categoryId` INT NOT NULL,
+	`lang_id` INT NOT NULL,
+	`category_id` INT NOT NULL,
 	`title` varchar(254) NOT NULL,
 	`name` varchar(254) NOT NULL
     
