d5ba5493ad1e20b92509d7dd55583dea5fee50fc
David Yang <daworld.ny@gmail.com>
2016-01-28 04:34:54 +0500

    Fixed wrong column name

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS `bono_module_videogallery_categories`;
 CREATE TABLE `bono_module_videogallery_categories` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`lang_id` INT NOT NULL,
-	`category_id` INT NOT NULL,
+	`parent_id` INT NOT NULL,
 	`title` varchar(254) NOT NULL,
 	`description` TEXT NOT NULL
     
