0144dc2959f6aac4196e722478e55df8aee7e207
David Yang <daworld.ny@gmail.com>
2016-02-03 11:57:36 +0500

    Changed module icon

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,7 @@ return array(
     'module' => array(
         'name'  => 'Videogallery',
         'url'   => '/admin/module/videogallery',
-        'icon'  => '/module/Videogallery/Assets/video.png',
+        'icon'  => 'fa fa-video-camera fa-5x',
         'order' => 1,
         'description' => 'Videogallery module allows you to make a video gallery on your web site'
     )
