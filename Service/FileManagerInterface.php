660ce9fc72632017d746a7ab151304d67a64ae5c
David Yang <daworld.ny@gmail.com>
2016-02-21 12:30:39 +0500

    Added contract methods

--- Service/FileManagerInterface.php
+++ Service/FileManagerInterface.php
@@ -13,5 +13,84 @@ namespace Videogallery\Service;
 
 interface FileManagerInterface
 {
-    
+    /**
+     * Update orders
+     * 
+     * @param array $orders
+     * @return boolean
+     */
+    public function updateOrders(array $orders);
+
+    /**
+     * Update published states
+     * 
+     * @param array $published
+     * @return boolean
+     */
+    public function updatePublished(array $published);
+
+    /**
+     * Update SEO states
+     * 
+     * @param array $seo
+     * @return boolean
+     */
+    public function updateSeo(array $seo);
+
+    /**
+     * Returns paginator instance
+     * 
+     * @return \Krystal\Paginate\Paginator
+     */
+    public function getPaginator();
+
+    /**
+     * Returns last id
+     * 
+     * @return integer
+     */
+    public function getLastId();
+
+    /**
+     * Adds a video file
+     * 
+     * @param array $input Raw input data
+     * @return boolean
+     */
+    public function add(array $input);
+
+    /**
+     * Updates a video file
+     * 
+     * @param array $input Raw input data
+     * @return boolean
+     */
+    public function update(array $input);
+
+    /**
+     * Fetches a video file info by its associated id
+     * 
+     * @param string $id
+     * @return array
+     */
+    public function fetchById($id);
+
+    /**
+     * Deletes a video file by its associated id
+     * 
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteById($id);
+
+    /**
+     * Fetch all records filtered by pagination
+     * 
+     * @param integer $page
+     * @param integer $itemsPerPage
+     * @param boolean $published Whether to fetch only published records
+     * @param string $categoryId Optionally can be filtered by category id
+     * @return array
+     */
+    public function fetchAllByPage($page, $itemsPerPage, $published, $categoryId = null);
 }
