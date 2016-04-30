77c9b69850ad8630f8ade2092a345ea048f6d118
David Yang <daworld.ny@gmail.com>
2016-04-30 22:38:53 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -3,7 +3,7 @@
 return array(
     'module' => array(
         'name'  => 'Videogallery',
-        'url'   => '/admin/module/videogallery',
+        'route' => 'Videogallery:Admin:Browser@indexAction',
         'icon'  => 'fa fa-video-camera fa-5x',
         'order' => 1,
         'description' => 'Videogallery module allows you to make a video gallery on your web site'
