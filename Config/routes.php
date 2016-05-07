d98a88b28601f887d15ec63fc47f25c7b1f56c0a
David Yang <daworld.ny@gmail.com>
2016-05-07 22:21:34 +0500

    Replaced static admin segment with its placeholder

--- Config/routes.php
+++ Config/routes.php
@@ -10,53 +10,53 @@
  */
 
 return array(
-    '/admin/module/videogallery' => array(
+    '/%s/module/videogallery' => array(
         'controller' => 'Admin:Browser@indexAction'
     ),
 
-    '/admin/module/videogallery/tweak' => array(
+    '/%s/module/videogallery/tweak' => array(
         'controller' => 'Admin:File@tweakAction',
         'disallow' => array('guest')
     ),
 
-    '/admin/module/videogallery/video/delete' => array(
+    '/%s/module/videogallery/video/delete' => array(
         'controller' => 'Admin:File@deleteAction',
         'disallow' => array('guest')
     ),
     
-    '/admin/module/videogallery/page/(:var)' => array(
+    '/%s/module/videogallery/page/(:var)' => array(
         'controller' => 'Admin:Browser@indexAction'
     ),
     
-    '/admin/module/videogallery/category/view/(:var)' => array(
+    '/%s/module/videogallery/category/view/(:var)' => array(
         'controller' => 'Admin:Browser@categoryAction'
     ),
 
-    '/admin/module/videogallery/category/(:var)/page/(:var)' => array(
+    '/%s/module/videogallery/category/(:var)/page/(:var)' => array(
         'controller' => 'Admin:Browser@categoryAction'
     ),
     
-    '/admin/module/videogallery/category/add' => array(
+    '/%s/module/videogallery/category/add' => array(
         'controller' => 'Admin:Category@addAction'
     ),
 
-    '/admin/module/videogallery/category/edit/(:var)' => array(
+    '/%s/module/videogallery/category/edit/(:var)' => array(
         'controller' => 'Admin:Category@editAction'
     ),
 
-    '/admin/module/videogallery/category/save' => array(
+    '/%s/module/videogallery/category/save' => array(
         'controller' => 'Admin:Category@saveAction'
     ),
     
-    '/admin/module/videogallery/file/add' => array(
+    '/%s/module/videogallery/file/add' => array(
         'controller' => 'Admin:File@addAction'
     ),
     
-    '/admin/module/videogallery/file/edit/(:var)' => array(
+    '/%s/module/videogallery/file/edit/(:var)' => array(
         'controller' => 'Admin:File@editAction'
     ),
     
-    '/admin/module/videogallery/file/save' => array(
+    '/%s/module/videogallery/file/save' => array(
         'controller' => 'Admin:File@saveAction'
     )
 );
