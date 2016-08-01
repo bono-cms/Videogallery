2b661fd82ea4c6d62180fff70e200621dc6688d4
David Yang <daworld.ny@gmail.com>
2016-08-01 22:30:20 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name'  => 'Videogallery',
-        'route' => 'Videogallery:Admin:Browser@indexAction',
-        'icon'  => 'fa fa-video-camera fa-5x',
-        'order' => 1,
-        'description' => 'Videogallery module allows you to make a video gallery on your web site'
-    )
+ 'name'  => 'Videogallery',
+ 'route' => 'Videogallery:Admin:Browser@indexAction',
+ 'icon'  => 'fa fa-video-camera fa-5x',
+ 'order' => 1,
+ 'description' => 'Videogallery module allows you to make a video gallery on your web site'
 );
